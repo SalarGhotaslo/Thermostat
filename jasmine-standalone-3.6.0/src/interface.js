5dedfe37efea9b85a1fd4fde35429fd7cb5c9129
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-11 17:39:12 +0000

    added last jqery and css style for energy usage

--- jasmine-standalone-3.6.0/src/interface.js
+++ jasmine-standalone-3.6.0/src/interface.js
@@ -5,35 +5,34 @@ $(document).ready(function () {
     $('#up').on('click', function () {
         thermostat.up(1);
         updateTemperature();
-    });
+      });
 
     $('#down').click(function () {
         thermostat.down(1);
         updateTemperature();
-    });
+      });
 
     $('#temperature-reset').click(function () {
         thermostat.reset();
         updateTemperature();
-    });
+      });
 
     $('#powerSavingModeOn').click(function () {
         thermostat.powerSavingModeOn();
         $('#power-saving-mode').text('on');
         updateTemperature();
-    });
+      });
 
     $('#powerSavingModeOff').click(function () {
         thermostat.powerSavingModeOff();
-        $('#power-saving-mode').text('off')
+        $('#power-saving-mode').text('off');
         updateTemperature();
-    });
-
+      });
 
     function updateTemperature() {
-        $('#current-temperature').text(thermostat.currentTemperature());
-        $('#power-usage').text(thermostat.energyUsage());
+      $('#current-temperature').text(thermostat.currentTemperature());
+      $('#power-usage').text(thermostat.energyUsage());
+      $('#power-usage').attr('class', thermostat.energyUsage());
     }
 
-
-});
+  });
