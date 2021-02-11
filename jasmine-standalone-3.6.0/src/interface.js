ecdbd1d0a270d7ce1ec7598a3fd0b357a59562d3
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-11 16:55:35 +0000

    added jquery for up and down temp

--- jasmine-standalone-3.6.0/src/interface.js
+++ jasmine-standalone-3.6.0/src/interface.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
-    let thermostat = new Thermostat
-    $('#current-temperature').text(thermostat.currentTemperature());
-});
+    let thermostat = new Thermostat;
+    updateTemperature();
+
+    $('#up').on('click', function () {
+      thermostat.up(1);
+      updateTemperature();
+    });
+
+    $('#down').click(function () {
+      thermostat.down(1);
+      updateTemperature();
+    });
+
+    function updateTemperature() {
+      $('#current-temperature').text(thermostat.currentTemperature());
+    }
+
+  });
