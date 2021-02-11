c94f3475344baa31ffe387beb013b88596fc0e26
Salar Ghotaslo <Salar_@live.co.uk>
2021-02-11 17:12:03 +0000

    added functions to jQuery

--- jasmine-standalone-3.6.0/src/interface.js
+++ jasmine-standalone-3.6.0/src/interface.js
@@ -3,17 +3,37 @@ $(document).ready(function () {
     updateTemperature();
 
     $('#up').on('click', function () {
-      thermostat.up(1);
-      updateTemperature();
+        thermostat.up(1);
+        updateTemperature();
     });
 
     $('#down').click(function () {
-      thermostat.down(1);
-      updateTemperature();
+        thermostat.down(1);
+        updateTemperature();
     });
 
+    $('#temperature-reset').click(function () {
+        thermostat.reset();
+        updateTemperature();
+    });
+
+    $('#powerSavingModeOn').click(function () {
+        thermostat.powerSavingModeOn();
+        $('#power-saving-mode').text('on');
+        updateTemperature();
+    });
+
+    $('#powerSavingModeOff').click(function () {
+        thermostat.powerSavingModeOff();
+        $('#power-saving-mode').text('off')
+        updateTemperature();
+    });
+
+
     function updateTemperature() {
-      $('#current-temperature').text(thermostat.currentTemperature());
+        $('#current-temperature').text(thermostat.currentTemperature());
+        $('#power-usage').text(thermostat.energyUsage());
     }
 
-  });
+
+});
--- jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine-standalone-3.6.0/src/thermostat.js
@@ -1,61 +1,62 @@
 'use strict';
 
 class Thermostat {
-  constructor() {
-    this.degrees = 20;
-    this.psm = true;
-  }
-
-  up(number) {
-    if ((this.degrees + number > 25) && this.psm === true) {
-      throw new Error('Save the planet, max temp of 25');
+    constructor() {
+        this.degrees = 20;
+        this.psm = true;
     }
 
-    if ((this.degrees + number) > 32 && this.psm === false) {
-      throw new Error('Max temp is 32');
+    up(number) {
+        if ((this.degrees + number > 25) && this.psm === true) {
+            throw new Error('Save the planet, max temp of 25');
+        }
+
+        if ((this.degrees + number) > 32 && this.psm === false) {
+            throw new Error('Max temp is 32');
+        }
+
+        this.degrees += number;
     }
 
-    this.degrees += number;
-  }
+    currentTemperature() {
+        return this.degrees;
 
-  currentTemperature() {
-    return this.degrees;
-
-  }
-
-  down(number) {
-    if ((this.degrees - number) < 10) {
-      throw new Error('This is too low bro!');
     }
 
-    this.degrees -= number;
-  }
+    down(number) {
+        if ((this.degrees - number) < 10) {
+            throw new Error('This is too low bro!');
+        }
 
-  powerSavingMode() {
-    return this.psm;
-  }
-
-  powerSavingModeOn() {
-    this.psm = true;
-  }
-
-  powerSavingModeOff() {
-    this.psm = false;
-  }
-
-  reset() {
-    this.degrees = 20;
-  }
-
-  energyUsage() {
-    if (this.degrees < 18) {
-      return `low usage`;
+        this.degrees -= number;
     }
 
-    if (this.degrees > 25) {
-      return `high usage`;
+    powerSavingMode() {
+        return this.psm;
     }
 
-    return `medium usage`;
-  }
+    powerSavingModeOn() {
+        this.psm = true;
+        return this.reset();
+    }
+
+    powerSavingModeOff() {
+        this.psm = false;
+    }
+
+    reset() {
+        this.degrees = 20;
+    }
+
+    energyUsage() {
+        if (this.degrees < 18) {
+            return `low usage`;
+        }
+
+        if (this.degrees > 25) {
+            return `high usage`;
+        }
+
+        return `medium usage`;
+    }
 }
